be3223b5740ef9e8cc8ec7d2cd42c8fb3b6dd766
Richard Pitts <Richardepitts@gmail.com>
2019-10-23 13:46:49 -0500

    -

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import store from './store'
 class App extends Component {
   render() {
   return (
+    <Router>
     <Provider store={store}>
     <div className="App">
       <header className="App-header">
@@ -25,6 +26,7 @@ class App extends Component {
       </header>
     </div>
     </Provider>
+    </Router>
     );
   }
 }
